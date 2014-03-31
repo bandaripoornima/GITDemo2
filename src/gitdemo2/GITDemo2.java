be0ebaf96533d8871211f2b89c206db35f0c2374
S516465 <S516465@NW26269.nwmissouri.edu>
2014-03-30 20:43:53 -0500

    Initializing first

---
+++ src/gitdemo2/GITDemo2.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitdemo2;
+
+/**
+ *
+ * @author S516465
+ */
+public class GITDemo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic 
+        System.out.println("Initializing in netbeans first");
+    }
+}
